313374689b6f2e1a9e65d8d84215c93a3dfca40b
Uchencho <aloziekelechi17@gmail.com>
2020-08-31 08:58:35 +0100

    server address/port

--- app/utils/utils.go
+++ app/utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"log"
 	"os"
 )
 
@@ -8,6 +9,7 @@ func GetServerAddress() string {
 	const defaultServerAddress = "127.0.0.1:8000"
 	serverAddress, present := os.LookupEnv("SERVER_ADDRESS")
 	if present {
+		log.Println("Found default address, ", serverAddress)
 		return serverAddress
 	}
 	return defaultServerAddress
